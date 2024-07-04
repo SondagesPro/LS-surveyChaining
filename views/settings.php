394e41684a8c6d2a7092ff177347e0f56086e460
Denis Chenu <contact@sondages.pro>
2024-07-04 18:31:03 +0200

    [feature] LimeSurvey 6 version

--- views/settings.php
+++ views/settings.php
@@ -1,10 +1,24 @@
 <div class="row">
+  <?php echo CHtml::beginForm();?>
     <div class="col-lg-12 content-right">
-        <h3><?php echo $title ?></h3>
+        <div class="h3 clearfix"><strong class="h4"><?php echo $title ?></strong>
+              <div class='pull-right hidden-xs'>
+              <?php
+              echo CHtml::htmlButton('<i class="fa fa-check" aria-hidden="true"></i> '.gT('Save'),array('type'=>'submit','name'=>'save'.$pluginClass,'value'=>'save','class'=>'btn btn-primary'));
+              echo " ";
+              echo CHtml::htmlButton('<i class="fa fa-check-circle-o " aria-hidden="true"></i> '.gT('Save and close'),array('type'=>'submit','name'=>'save'.$pluginClass,'value'=>'redirect','class'=>'btn btn-default btn-secondary'));
+              echo " ";
+              echo CHtml::link(
+                gT('Close'),
+                (floatval(App()->getConfig('versionnumber')) < 4) ? App()->createUrl('admin/survey',array('sa'=>'view','surveyid'=>$surveyId)) : App()->createUrl('surveyAdministration/view', array('surveyid'=>$surveyId)),
+                array('class'=>'btn btn-danger')
+              );
+              ?>
+              </div>
+        </div>
         <?php if($warningString) {
             echo CHtml::tag("p",array('class'=>'alert alert-warning'),$warningString);
         } ?>
-        <?php echo CHtml::beginForm();?>
         <?php foreach($aSettings as $legend=>$settings) {
           $this->widget('ext.SettingsWidget.SettingsWidget', array(
                 //'id'=>'summary',
@@ -20,29 +34,23 @@
             ));
         } ?>
         <div class='row'>
-          <div class='col-md-offset-6 submit-buttons'>
+          <div class='col-md-6'></div>
+          <div class='col-md-6 submit-buttons'>
             <?php
               echo CHtml::htmlButton('<i class="fa fa-check" aria-hidden="true"></i> '.gT('Save'),array('type'=>'submit','name'=>'save'.$pluginClass,'value'=>'save','class'=>'btn btn-primary'));
               echo " ";
-              echo CHtml::htmlButton('<i class="fa fa-check-circle-o " aria-hidden="true"></i> '.gT('Save and close'),array('type'=>'submit','name'=>'save'.$pluginClass,'value'=>'redirect','class'=>'btn btn-default'));
+              echo CHtml::htmlButton('<i class="fa fa-check-circle-o " aria-hidden="true"></i> '.gT('Save and close'),array('type'=>'submit','name'=>'save'.$pluginClass,'value'=>'redirect','class'=>'btn btn-default btn-secondary'));
               echo " ";
-              echo CHtml::link(gT('Close'),Yii::app()->createUrl('admin/survey',array('sa'=>'view','surveyid'=>$surveyId)),array('class'=>'btn btn-danger'));
+              echo CHtml::link(
+                gT('Close'),
+                (floatval(App()->getConfig('versionnumber')) < 4) ? App()->createUrl('admin/survey',array('sa'=>'view','surveyid'=>$surveyId)) : App()->createUrl('surveyAdministration/view', array('surveyid'=>$surveyId)),
+                array('class'=>'btn btn-danger')
+              );
             ?>
-            <div class='hidden' style='display:none'>
-              <div data-moveto='surveybarid' class='pull-right hidden-xs'>
-              <?php
-              echo CHtml::link('<i class="fa fa-check" aria-hidden="true"></i> '.gT('Save'),"#",array('class'=>'btn btn-primary','data-click-name'=>'save'.$pluginClass,'data-click-value'=>'save'));
-              echo " ";
-              echo CHtml::link('<i class="fa fa-check-circle-o" aria-hidden="true"></i> '.gT('Save and close'),"#",array('class'=>'btn btn-default','data-click-name'=>'save'.$pluginClass,'data-click-value'=>'redirect'));
-              echo " ";
-              echo CHtml::link(gT('Close'),Yii::app()->createUrl('admin/survey',array('sa'=>'view','surveyid'=>$surveyId)),array('class'=>'btn btn-danger'));
-              ?>
-              </div>
-            </div>
           </div>
         </div>
-        <?php echo CHtml::endForm();?>
     </div>
+  <?php echo CHtml::endForm();?>
 </div>
 <?php
   Yii:app()->clientScript->registerScriptFile($assetUrl.'/settings.js',CClientScript::POS_END);
